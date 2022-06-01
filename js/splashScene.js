faa014abd948522d64960cbc96fe671c34676138
Angelo.Pintilie <99273785+Angelo-Pintilie@users.noreply.github.com>
2022-06-01 14:31:05 +0000

    js finished

---
+++ js/splashScene.js
@@ -0,0 +1,38 @@
+/* global Phaser */
+
+// Copyright (c) 2022 Angelo Pintilie All rights reserved
+//
+// Created by: Angelo Pintilie
+// Created on: June 2022
+// This is the Splash Scene
+
+class SplashScene extends Phaser.Scene {
+  constructor () {
+    super({ key: 'splashScene' })
+
+    this.splashSceneBackgroundImage = null
+  }
+
+  init (data) {
+    this.cameras.main.setBackgroundColor('#ffffff')
+  }
+
+  preload () {
+    console.log('Splash Scene')
+    this.load.image('splashSceneBackground', './assets/splashSceneImage.png')
+  }
+
+  create (data) {
+    this.splashSceneBackgroundImage = this.add.sprite(0, 0, 'splashSceneBackground')
+    this.splashSceneBackgroundImage.x = 1920 / 2
+    this.splashSceneBackgroundImage.y = 1080 / 2
+  }
+
+  update (time, delta) {
+    if (time > 3000) {
+      this.scene.switch('titleScene')
+    }
+  }
+}
+
+export default SplashScene
