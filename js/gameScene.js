0bc88e1b420ce1bcb855d78ac157de67ece5a928
Angelo.Pintilie <99273785+Angelo-Pintilie@users.noreply.github.com>
2022-06-08 01:25:09 +0000

    boundaries work

--- js/gameScene.js
+++ js/gameScene.js
@@ -41,15 +41,18 @@ class GameScene extends Phaser.Scene {
     const keyRightObj = this.input.keyboard.addKey('RIGHT')
 
     if (keyLeftObj.isDown === true) {
-      this.ship.x = this.ship.x - 15
+      this.ship.x -= 15
       if (this.ship.x < 0) {
         this.ship.x = 0
       }
     }
 
     if (keyRightObj.isDown === true) {
-      this.ship.x = this.ship.x + 15
-    }
+      this.ship.x += 15
+      if (this.ship.x > 1920) {
+        this.ship.x = 1920
+      }
+    }  
   }
 }
 
